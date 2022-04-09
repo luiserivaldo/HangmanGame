c3c7defcc89556152d60dfe89642335df9cee247
luise <luiserivaldo99@gmail.com>
2022-04-09 16:30:41 +0700

    Add project files.

---
+++ HangmanGame/HangmanGame.cpp
@@ -0,0 +1,18 @@
+// HangmanGame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to Hangman!" << std::endl;
+    std::cout << " _________" << std::endl;
+    std::cout << "|    |    " << std::endl;
+    std::cout << "|    O    " << std::endl;
+    std::cout << "|  / | \\  " << std::endl;
+    std::cout << "|    |     " << std::endl;
+    std::cout << "|   / \\   " << std::endl;
+    std::cout << "|          " << std::endl;
+    std::cout << "|_________" << std::endl;
+}
+
