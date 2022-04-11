6eee238945fdda40e9603ef6ddc84f09426af909
luise <luiserivaldo99@gmail.com>
2022-04-12 05:41:29 +0700

    add hangman constructor loop based on tries left

--- HangmanGame/HangmanGame.cpp
+++ HangmanGame/HangmanGame.cpp
@@ -2,17 +2,17 @@
 //
 
 #include <iostream>
+#include <fstream>
 
-int hangmanVisual() {
-    // Hangman Visual constructor. Change the char to reflect tries left.
-    char hangmanHead = 'O';
-    char hangmanLeftArm = '/';
-    char hangmanUpperBody = '|';
-    char hangmanRightArm = '\\';
-    char hangmanLowerBody = '|';
-    char hangmanLeftLeg = '/';
-    char hangmanRightLeg = '\\';
-
+void hangmanVisual () {
+    // Base Hangman Visual constructor.
+    char hangmanHead = ' ';
+    char hangmanLeftArm = ' ';
+    char hangmanUpperBody = ' ';
+    char hangmanRightArm = ' ';
+    char hangmanLowerBody = ' ';
+    char hangmanLeftLeg = ' ';
+    char hangmanRightLeg = ' ';
 
     std::cout << " _________" << std::endl;
     std::cout << "|    |    " << std::endl;
@@ -22,23 +22,50 @@ int hangmanVisual() {
     std::cout << "|   " << hangmanLeftLeg << " " << hangmanRightLeg << " " << std::endl;
     std::cout << "|         " << std::endl;
     std::cout << "|_________" << std::endl;
-    return 0;
 }
 
 int gameplay() {
+    // Hangman Visual constructor. Change the char to reflect tries left.
+    int triesLeft = 7;
+    if (triesLeft <= 6) {
+        char hangmanHead = 'O';
+    }
+    else if (triesLeft <= 5) {
+        char hangmanLeftArm = '/';
+    }
+    else if (triesLeft <= 4) {
+        char hangmanUpperBody = '|';
+    }
+    else if (triesLeft <= 3) {
+        char hangmanRightArm = '\\';
+    }
+    else if (triesLeft <= 2) {
+        char hangmanLowerBody = '|';
+    }
+    else if (triesLeft <= 1) {
+        char hangmanLeftLeg = '/';
+    }
+    else if (triesLeft <= 0) {
+        char hangmanRightLeg = '\\';
+    }
+
     // Player has 7 tries to complete the game.
-    int triesLeft = 6;
+    hangmanVisual();
     while (triesLeft > 0) {
         hangmanVisual();
+        std::cout << "\nYou have " << triesLeft << " tries left!" << std::endl;
         int triesleft = triesLeft--;
-        std::cout << triesLeft;
     }
+    if (triesLeft <= 0) {
+        hangmanVisual();
+        std::cout << "\nNo tries left. Game over!";
+    }
+
     return 0;
 }
 
 int main() {
     std::cout << "Welcome to Hangman!" << std::endl;
-    hangmanVisual();
     gameplay();
 }
 
