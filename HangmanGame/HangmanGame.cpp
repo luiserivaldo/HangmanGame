1d027746b883babe6214b51d101bc7515fd4e2dd
luise <luiserivaldo99@gmail.com>
2022-04-13 15:34:52 +0700

    add conditional visual constructur depending on tries left

--- HangmanGame/HangmanGame.cpp
+++ HangmanGame/HangmanGame.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <fstream>
 
-void hangmanVisual () {
-    // Base Hangman Visual constructor.
+int gameplay() {
+    // Empty Hangman Visual constructor.
     char hangmanHead = ' ';
     char hangmanLeftArm = ' ';
     char hangmanUpperBody = ' ';
@@ -14,53 +14,57 @@ void hangmanVisual () {
     char hangmanLeftLeg = ' ';
     char hangmanRightLeg = ' ';
 
-    std::cout << " _________" << std::endl;
-    std::cout << "|    |    " << std::endl;
-    std::cout << "|    " << hangmanHead << "   "<< std::endl;
-    std::cout << "|  " << hangmanLeftArm << " " << hangmanUpperBody << " " << hangmanRightArm << " " << std::endl;
-    std::cout << "|    " << hangmanLowerBody << "     " << std::endl;
-    std::cout << "|   " << hangmanLeftLeg << " " << hangmanRightLeg << " " << std::endl;
-    std::cout << "|         " << std::endl;
-    std::cout << "|_________" << std::endl;
-}
-
-int gameplay() {
-    // Hangman Visual constructor. Change the char to reflect tries left.
+    // Number of tries the player has.
     int triesLeft = 7;
-    if (triesLeft <= 6) {
-        char hangmanHead = 'O';
-    }
-    else if (triesLeft <= 5) {
-        char hangmanLeftArm = '/';
-    }
-    else if (triesLeft <= 4) {
-        char hangmanUpperBody = '|';
-    }
-    else if (triesLeft <= 3) {
-        char hangmanRightArm = '\\';
-    }
-    else if (triesLeft <= 2) {
-        char hangmanLowerBody = '|';
-    }
-    else if (triesLeft <= 1) {
-        char hangmanLeftLeg = '/';
-    }
-    else if (triesLeft <= 0) {
-        char hangmanRightLeg = '\\';
+
+    while (triesLeft > 0) {
+        std::cout << " _________" << std::endl;
+        std::cout << "|    |    " << std::endl;
+        std::cout << "|    " << hangmanHead << "   " << std::endl;
+        std::cout << "|  " << hangmanLeftArm << " " << hangmanUpperBody << " " << hangmanRightArm << " " << std::endl;
+        std::cout << "|    " << hangmanLowerBody << "     " << std::endl;
+        std::cout << "|   " << hangmanLeftLeg << " " << hangmanRightLeg << " " << std::endl;
+        std::cout << "|         " << std::endl;
+        std::cout << "|_________" << std::endl;
+        std::cout << "\nYou have " << triesLeft << " tries left!" << std::endl;
+
+        int triesleft = triesLeft--;
+
+        // Change Hangman visual depending on tries left.
+        if (triesLeft <= 6) {
+            hangmanHead = 'O';
+        }
+        if (triesLeft <= 5) {
+            hangmanUpperBody = '|';
+        }
+        if (triesLeft <= 4) {
+            hangmanLeftArm = '/';
+        }
+        if (triesLeft <= 3) {
+            hangmanRightArm = '\\';
+        }
+        if (triesLeft <= 2) {
+            hangmanLowerBody = '|';
+        }
+        if (triesLeft <= 1) {
+            hangmanLeftLeg = '/';
+        }
+        if (triesLeft <= 0) {
+            hangmanRightLeg = '\\';
+        }
     }
 
-    // Player has 7 tries to complete the game.
-    hangmanVisual();
-    while (triesLeft > 0) {
-        hangmanVisual();
-        std::cout << "\nYou have " << triesLeft << " tries left!" << std::endl;
-        int triesleft = triesLeft--;
-    }
     if (triesLeft <= 0) {
-        hangmanVisual();
+        std::cout << " _________" << std::endl;
+        std::cout << "|    |    " << std::endl;
+        std::cout << "|    " << hangmanHead << "   " << std::endl;
+        std::cout << "|  " << hangmanLeftArm << " " << hangmanUpperBody << " " << hangmanRightArm << " " << std::endl;
+        std::cout << "|    " << hangmanLowerBody << "     " << std::endl;
+        std::cout << "|   " << hangmanLeftLeg << " " << hangmanRightLeg << " " << std::endl;
+        std::cout << "|         " << std::endl;
+        std::cout << "|_________" << std::endl;
         std::cout << "\nNo tries left. Game over!";
     }
-
     return 0;
 }
 
